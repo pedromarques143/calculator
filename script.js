4bdbdb0d98178c8dc0e9534acefc0682b49f2078
pedromarques143 <pedromarques.trad@gmail.com>
2024-07-10 01:12:50 +0100

    Added functions

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 let firstNumber = "";
 let selectedOperator;
-let secondNumber;
+let secondNumber = "";
 
 let displayValue = 0;
 let calculatorScreen = document.querySelector(".calculator-screen");
 
 let firstNumberExists = false;
+let secondNumberExists = false;
 let dotExists = false;
+let resultExists = false;
+let isOperatorSelected = false;
 
 //get first number
 const calculatorNumbers = document.querySelectorAll(".calculator-button-number");
 
 for (let button of calculatorNumbers) {
     button.addEventListener("click", () => {
-        if (firstNumber.length < 9) {
-            if (button.textContent == "." && !dotExists && firstNumberExists == false) {
+        if (firstNumber.length < 9 && !isOperatorSelected) {
+            if (button.textContent == "." && !dotExists && !firstNumberExists) {
                 firstNumber = "0.";
                 dotExists = true;
                 firstNumberExists = true;
                 calculatorScreen.textContent = firstNumber;
-            } else if (button.textContent == "." && dotExists && firstNumberExists) {
+            } else if (button.textContent == "." && !dotExists) {
+                firstNumber += button.textContent;
+                dotExists = true;
+                calculatorScreen.textContent = firstNumber;
+            } else if (button.textContent == ".") {
                 firstNumber.slice(0, -1);
             } else {
                 firstNumber += button.textContent;
@@ -31,8 +38,77 @@ for (let button of calculatorNumbers) {
 }
 
 //get operator
-const calculatorOperator = document.querySelectorAll(".rABC")
+const calculatorOperators = document.querySelectorAll(".calculator-button-operator");
 
-function operate(firstNumber, operator, secondNumber) {
-
+for (let button of calculatorOperators) {
+    button.addEventListener("click", () => {
+        if (firstNumberExists && !isOperatorSelected) {
+            selectedOperator = button.textContent;
+            button.style.backgroundColor = "darkolivegreen";
+            isOperatorSelected = true;
+            dotExists = false;
+        }
+    });
 }
+
+//get second number
+for (let button of calculatorNumbers) {
+    button.addEventListener("click", () => {
+        if (isOperatorSelected == true) {
+            if (button.textContent == "." && !dotExists && !secondNumberExists) {
+                secondNumber = "0.";
+                dotExists = true;
+                calculatorScreen.textContent = secondNumber;
+                secondNumberExists = true;
+            } else if (button.textContent == "." && !dotExists) {
+                secondNumber += button.textContent;
+                dotExists = true;
+                calculatorScreen.textContent = secondNumber;
+            } else if (button.textContent == ".") {
+                secondNumber.slice(0, -1);
+            } else {
+                secondNumber += button.textContent;
+                calculatorScreen.textContent = secondNumber;
+                secondNumberExists = true;
+            }
+
+            for (let buttonTwo of calculatorOperators) {
+                buttonTwo.style.backgroundColor = "darkseagreen";
+            }
+        }
+        isOperatorSelected = false;
+    });
+}
+
+//equals button
+const equalsButton = document.querySelector(".calculator-button-equals");
+
+equalsButton.addEventListener("click", () => {
+    operate(firstNumber, secondNumber, selectedOperator);
+});
+
+
+//get result
+function operate(first, second, operator) {
+    let numberOne = Number(first);
+    let numberTwo = Number(second);
+    let result;
+
+    if (operator == "+") {
+        result = numberOne + numberTwo;
+        calculatorScreen.textContent = result;
+        secondNumberExists = false;
+        firstNumber = result;
+    }
+
+    return result;
+}
+
+
+//subtract
+
+//muiltiply
+
+//divide
+
+//reset button
